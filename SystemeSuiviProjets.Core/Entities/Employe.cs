42a7a62774131e278cb39c8082791e614d10d042
FabienTri <airaudfabien@gmail.com>
2023-12-15 15:09:40 -0500

    Create Utilisateur Employe

---
+++ SystemeSuiviProjets.Core/Entities/Employe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class Employe : Utilisateur
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Adresse { get; set; }
+        public string Telephone { get; set; }
+        public DateOnly DateEmbauche { get; set; }
+    }
+}
