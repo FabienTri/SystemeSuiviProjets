7a99a5bc85323b225bd07971af5670979cb00a31
FabienTri <airaudfabien@gmail.com>
2023-12-15 13:00:01 -0500

    Create entity Categorie

---
+++ SystemeSuiviProjets.Core/Entities/Categorie.cs
@@ -0,0 +1,15 @@
+﻿using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class Categorie : BaseEntity
+    {
+        public string Description { get; set; }
+        public long TarifFacturationHeure { get; set; }
+        public long TarifFacturationJour { get; set; }
+        public long TarifFacturationSemaine { get; set; }
+        public long TarifPaiementHeure { get; set; }
+        public long TarifPaiementJour { get; set; }
+        public long TarifPaiementSemaine { get; set; }
+    }
+}
