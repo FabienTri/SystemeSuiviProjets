76b9e19da6a73db10268bbebfa36136dd62e1118
FabienTri <airaudfabien@gmail.com>
2023-12-15 14:52:33 -0500

    Create entity Utilisateur

---
+++ SystemeSuiviProjets.Core/Entities/Utilisateur.cs
@@ -0,0 +1,10 @@
+﻿using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class Utilisateur : BaseEntity
+    {
+        public string NomConnexion { get; set; }
+        public string MotDePasse { get; set; }
+    }
+}
