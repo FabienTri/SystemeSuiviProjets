da542da2d91ffa0bf6d2668b1730dcb0d426885f
FabienTri <airaudfabien@gmail.com>
2023-12-15 15:24:53 -0500

    Create Utilisateur Client

---
+++ SystemeSuiviProjets.Core/Entities/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class Client : Utilisateur
+    {
+        public string NomCompagnie { get; set; }
+        public string Adresse { get; set; }
+        public string Telephone { get; set; }
+    }
+}
