7dc26f2b57860b218885a40773c5c97dedf34de9
FabienTri <airaudfabien@gmail.com>
2023-12-16 18:02:26 -0500

    Create Entity FeuillePrésence

---
+++ SystemeSuiviProjets.Core/Entities/FeuillePrésence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class FeuillePrésence(DateRange période, DateTime dateCréation) : BaseEntity
+    {
+        public DateRange Période { get; set; } = période;
+        public DateTime DateCréation { get; set; } = dateCréation;
+        public DateTime DateDernièreModification { get; set; } = dateCréation;
+    }
+}
