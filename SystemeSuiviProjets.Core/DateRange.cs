5b6f1c8a3fc87aad32397cde15409837a5f90225
FabienTri <airaudfabien@gmail.com>
2023-12-16 17:47:39 -0500

    Create ValueObject DateRange

---
+++ SystemeSuiviProjets.Core/DateRange.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    internal class DateRange(DateTime dateDébut, DateTime dateFin) : ValueObject
+    {
+        public DateTime DateDébut { get; set; } = dateDébut;
+        public DateTime DateFin { get; set; } = dateFin;
+    }
+}
