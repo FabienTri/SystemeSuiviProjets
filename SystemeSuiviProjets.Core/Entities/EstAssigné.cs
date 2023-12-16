23fc262b50767fd8e678e0452153e344cce12071
FabienTri <airaudfabien@gmail.com>
2023-12-16 14:57:03 -0500

    Create Entity EstAssigné

---
+++ SystemeSuiviProjets.Core/Entities/EstAssigné.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class EstAssigné : BaseEntity
+    {
+        public DateOnly DateAssignation { get; set; }
+    }
+}
