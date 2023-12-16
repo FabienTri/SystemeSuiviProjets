33b5858edeb76dcfdb9afff9d5ca45f1faaedbb4
FabienTri <airaudfabien@gmail.com>
2023-12-16 18:47:32 -0500

    Create Entity LigneFeuillePrésence

---
+++ SystemeSuiviProjets.Core/Entities/LigneFeuillePrésence.cs
@@ -0,0 +1,12 @@
+﻿using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class LigneFeuillePrésence(TypeTarification typeTarification, int nbUnités, decimal fraisDéplacement = 0, decimal fraisSéjour = 0) : BaseEntity
+    {
+        public TypeTarification TypeTarification { get; set; } = typeTarification;
+        public int NombreUnités { get; set; } = nbUnités;
+        public decimal FraisDéplacement { get; set; } = fraisDéplacement;
+        public decimal FraisSéjour { get; set; } = fraisSéjour;
+    }
+}
