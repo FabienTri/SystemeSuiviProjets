aafa4f7a6eeb7dd7674e5cc85b4ce046fb60dc13
FabienTri <airaudfabien@gmail.com>
2023-12-16 14:55:02 -0500

    Create Entity ProfessionnelEstDansCatégorie

---
+++ SystemeSuiviProjets.Core/Entities/ProfessionnelEstDansCatégorie.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class ProfessionnelEstDansCatégorie : BaseEntity
+    {
+        public DateOnly DateAffectation { get; set; }
+    }
+}
