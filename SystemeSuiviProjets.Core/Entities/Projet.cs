1f4e964deeea246a57c7902003b07cd107ac7c63
FabienTri <airaudfabien@gmail.com>
2023-12-16 14:54:50 -0500

    Create Entity Projet

---
+++ SystemeSuiviProjets.Core/Entities/Projet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class Projet : BaseEntity
+    {
+        public DateOnly DateDébut { get; set; }
+        public DateOnly DateFin { get; set; }
+        public long BudgetTotal { get; set; }
+    }
+}
