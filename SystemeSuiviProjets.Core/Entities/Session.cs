ebf8ffe89943ed7116e578475106b3f2e6d7b490
FabienTri <airaudfabien@gmail.com>
2023-12-16 17:47:22 -0500

    Create Entity Session

---
+++ SystemeSuiviProjets.Core/Entities/Session.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SystemeSuiviProjets.SharedKernel;
+
+namespace SystemeSuiviProjets.Core
+{
+    public class Session(DateTime dateConnexion) : BaseEntity
+    {
+        public DateTime DateConnexion { get; set; } = dateConnexion;
+        public DateTime DateDeconnexion { get; set; }
+    }
+}
