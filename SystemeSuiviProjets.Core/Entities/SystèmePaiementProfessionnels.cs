6bc001370c8729b18ce0343ed2c03208b7a0ae6e
FabienTri <airaudfabien@gmail.com>
2023-12-17 12:36:03 -0500

    Can't change Professionnel and can change FeuillesPrésences

--- SystemeSuiviProjets.Core/Entities/SystèmePaiementProfessionnels.cs
+++ SystemeSuiviProjets.Core/Entities/SystèmePaiementProfessionnels.cs
@@ -6,8 +6,16 @@ namespace SystemeSuiviProjets.Core
     public class SystèmePaiementProfessionnels(Professionnel professionnel) : BaseEntity
     {
         public int ProfessionnelId { get; set; } = professionnel.Id;
-        public Professionnel Professionnel { get; set; } = professionnel;
+        public Professionnel Professionnel { get; private set; } = professionnel;
 
-        public List<FeuillePrésence> FeuillesPrésence { get; set; } = [];
+        public List<FeuillePrésence> FeuillesPrésence { get; private set; } = [];
+        public void AddFeuillePrésence(FeuillePrésence feuillePrésence)
+        {
+            FeuillesPrésence.Add(feuillePrésence);
+        }
+        public void RemoveFeuillePrésence(FeuillePrésence feuillePrésence)
+        {
+            FeuillesPrésence.Remove(feuillePrésence);
+        }
     }
 }
