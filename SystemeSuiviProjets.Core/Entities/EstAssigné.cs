e414db11288650bf75debd03bc982a6a6b2c2b15
FabienTri <airaudfabien@gmail.com>
2023-12-17 12:04:40 -0500

    Professionnel and Projet in private for EstAssigné

--- SystemeSuiviProjets.Core/Entities/EstAssigné.cs
+++ SystemeSuiviProjets.Core/Entities/EstAssigné.cs
@@ -6,10 +6,10 @@ namespace SystemeSuiviProjets.Core
     public class EstAssigné(Professionnel professionnel, Projet projet, DateOnly dateAssignation) : BaseEntity
     {
         public int ProfessionnelId { get; set; } = professionnel.Id;
-        public Professionnel Professionnel { get; set; } = professionnel;
+        public Professionnel Professionnel { get; private set; } = professionnel;
 
         public int ProjetId { get; set; } = projet.Id;
-        public Projet Projet { get; set; } = projet;
+        public Projet Projet { get; private set; } = projet;
 
         public DateOnly DateAssignation { get; set; } = dateAssignation;
     }
