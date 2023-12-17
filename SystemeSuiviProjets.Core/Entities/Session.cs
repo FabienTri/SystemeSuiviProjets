cebbfec24dce990590b6d24c5c5b305f1218d442
FabienTri <airaudfabien@gmail.com>
2023-12-17 12:28:13 -0500

    Can't change Utilisateur and DateConnexion for Session

--- SystemeSuiviProjets.Core/Entities/Session.cs
+++ SystemeSuiviProjets.Core/Entities/Session.cs
@@ -6,9 +6,9 @@ namespace SystemeSuiviProjets.Core
     public class Session(Utilisateur utilisateur, DateTime dateConnexion) : BaseEntity
     {
         public int UtilisateurId { get; set; } = utilisateur.Id;
-        public Utilisateur Utilisateur { get; set; } = utilisateur;
+        public Utilisateur Utilisateur { get; private set; } = utilisateur;
 
-        public DateTime DateConnexion { get; set; } = dateConnexion;
+        public DateTime DateConnexion { get; private set; } = dateConnexion;
         public DateTime DateDeconnexion { get; set; }
     }
 }
