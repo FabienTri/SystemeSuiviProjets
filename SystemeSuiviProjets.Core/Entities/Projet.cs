c9268151b1aa8f8000299a88c1d9b4ecfb821247
FabienTri <airaudfabien@gmail.com>
2023-12-17 09:38:35 -0500

    Add Projet constructor

--- SystemeSuiviProjets.Core/Entities/Projet.cs
+++ SystemeSuiviProjets.Core/Entities/Projet.cs
@@ -3,10 +3,10 @@ using SystemeSuiviProjets.SharedKernel;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class Projet : BaseEntity
+    public class Projet(DateOnly dateDébut, long budgetTotal) : BaseEntity
     {
-        public DateOnly DateDébut { get; set; }
-        public DateOnly DateFin { get; set; }
-        public long BudgetTotal { get; set; }
+        public DateOnly DateDébut { get; set; } = dateDébut;
+        public DateOnly? DateFin { get; set; }
+        public long BudgetTotal { get; set; } = budgetTotal;
     }
 }
