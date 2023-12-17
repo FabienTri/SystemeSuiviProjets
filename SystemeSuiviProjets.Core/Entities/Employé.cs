41bfdcf76de47c4055baabe9a2f6515e028c8172
FabienTri <airaudfabien@gmail.com>
2023-12-17 09:30:03 -0500

    Add Employé constructor

--- SystemeSuiviProjets.Core/Entities/Employé.cs
+++ SystemeSuiviProjets.Core/Entities/Employé.cs
@@ -2,12 +2,12 @@
 
 namespace SystemeSuiviProjets.Core
 {
-    public class Employé : Utilisateur
+    public class Employé(string nom, string prénom, string adresse, string téléphone, DateOnly dateEmbauche) : Utilisateur
     {
-        public string Nom { get; set; }
-        public string Prénom { get; set; }
-        public string Adresse { get; set; }
-        public string Téléphone { get; set; }
-        public DateOnly DateEmbauche { get; set; }
+        public string Nom { get; set; } = nom;
+        public string Prénom { get; set; } = prénom;
+        public string Adresse { get; set; } = adresse;
+        public string Téléphone { get; set; } = téléphone;
+        public DateOnly DateEmbauche { get; set; } = dateEmbauche;
     }
 }
