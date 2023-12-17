fc1881613613222a8d174e2bfd04dce4eb655ea8
FabienTri <airaudfabien@gmail.com>
2023-12-17 11:13:06 -0500

    Add Projets in Client

--- SystemeSuiviProjets.Core/Entities/Client.cs
+++ SystemeSuiviProjets.Core/Entities/Client.cs
@@ -1,4 +1,6 @@
-﻿namespace SystemeSuiviProjets.Core
+﻿using System.Collections.Generic;
+
+namespace SystemeSuiviProjets.Core
 {
     public class Client(string nomConnexion, string motDePasse, string nomCompagnie, string adresse, string téléphone)
         : Utilisateur(nomConnexion, motDePasse)
@@ -6,5 +8,7 @@
         public string NomCompagnie { get; set; } = nomCompagnie;
         public string Adresse { get; set; } = adresse;
         public string Téléphone { get; set; } = téléphone;
+
+        public List<Projet> Projets { get; set; } = [];
     }
 }
