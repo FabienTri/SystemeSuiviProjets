d006e55280be3ec013cec6a23db873b435c36457
FabienTri <airaudfabien@gmail.com>
2023-12-17 18:24:33 -0500

    Change constructor to add an empty one for EF core

--- SystemeSuiviProjets.Core/Entities/FeuillePrésence.cs
+++ SystemeSuiviProjets.Core/Entities/FeuillePrésence.cs
@@ -4,18 +4,16 @@ using SystemeSuiviProjets.SharedKernel;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class FeuillePrésence(SystèmePaiementProfessionnels systèmePaiementProfessionnels,
-        DateRange période, DateTime dateCréation)
-        : BaseEntity
+    public class FeuillePrésence : BaseEntity
     {
-        public int SystèmePaiementProfessionnelsId { get; set; } = systèmePaiementProfessionnels.Id;
-        public SystèmePaiementProfessionnels SystèmePaiementProfessionnels { get; private set; } = systèmePaiementProfessionnels;
+        public int SystèmePaiementProfessionnelsId { get; set; }
+        public SystèmePaiementProfessionnels SystèmePaiementProfessionnels { get; private set; }
 
-        public DateRange Période { get; set; } = période;
-        public DateTime DateCréation { get; private set; } = dateCréation;
-        public DateTime DateDernièreModification { get; set; } = dateCréation;
+        public DateRange Période { get; set; }
+        public DateTime DateCréation { get; private set; }
+        public DateTime DateDernièreModification { get; set; }
 
-        public List<LigneFeuillePrésence> LignesFeuillePrésence { get; private set; } = [];
+        public List<LigneFeuillePrésence> LignesFeuillePrésence { get; private set; }
         public void AddLigneFeuillePrésence(LigneFeuillePrésence ligneFeuillePrésence)
         {
             LignesFeuillePrésence.Add(ligneFeuillePrésence);
@@ -23,6 +21,22 @@ namespace SystemeSuiviProjets.Core
         public void RemoveLigneFeuillePrésence(LigneFeuillePrésence ligneFeuillePrésence)
         {
             LignesFeuillePrésence.Remove(ligneFeuillePrésence);
+        }
+
+        public FeuillePrésence()
+        {
+            // EF Core needs this one to materialize collections from DB Set
+        }
+
+        public FeuillePrésence(SystèmePaiementProfessionnels systèmePaiementProfessionnels,
+            DateRange période, DateTime dateCréation)
+        {
+            SystèmePaiementProfessionnelsId = systèmePaiementProfessionnels.Id;
+            SystèmePaiementProfessionnels = systèmePaiementProfessionnels;
+            Période = période;
+            DateCréation = dateCréation;
+            DateDernièreModification = dateCréation;
+            LignesFeuillePrésence = [];
         }
     }
 }
