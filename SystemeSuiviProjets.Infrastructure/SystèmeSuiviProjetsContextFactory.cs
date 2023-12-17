4b8c497cf69f8f54d149d67e17b9c4eed20e44a7
FabienTri <airaudfabien@gmail.com>
2023-12-17 15:04:07 -0500

    Create SystèmeSuiviProjetsContextFactory

---
+++ SystemeSuiviProjets.Infrastructure/SystèmeSuiviProjetsContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace SystemeSuiviProjets.Infrastructure
+{
+    public class SystèmeSuiviProjetsContextFactory : IDesignTimeDbContextFactory<SystèmeSuiviProjetsContext>
+    {
+        public SystèmeSuiviProjetsContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SystèmeSuiviProjetsContext>();
+            optionsBuilder.UseSqlServer(@"Server=.;Database=SystèmeGestionStationServiceDB;Trusted_Connection=True;");
+
+            return new SystèmeSuiviProjetsContext(optionsBuilder.Options);
+        }
+    }
+}
