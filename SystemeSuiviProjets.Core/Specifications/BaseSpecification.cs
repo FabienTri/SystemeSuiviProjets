6300f342a8fb9809b00f74043dc211b1ce46e5a7
FabienTri <airaudfabien@gmail.com>
2023-12-17 16:51:15 -0500

    Correct import

--- SystemeSuiviProjets.Core/Specifications/BaseSpecification.cs
+++ SystemeSuiviProjets.Core/Specifications/BaseSpecification.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using SystèmeGestionStationService.SharedKernel.Interfaces;
+using SystemeSuiviProjets.SharedKernel.Interfaces;
 
 namespace SystèmeGestionStationService.Core.Specifications
 {
