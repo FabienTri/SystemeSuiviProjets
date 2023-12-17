2d5ea065efe167bb59b82965ed34dcd53f984ca0
FabienTri <airaudfabien@gmail.com>
2023-12-17 18:30:20 -0500

    Change constructor to add an empty one for EF core

--- SystemeSuiviProjets.Core/Entities/ProfessionnelEstDansCatégorie.cs
+++ SystemeSuiviProjets.Core/Entities/ProfessionnelEstDansCatégorie.cs
@@ -3,15 +3,29 @@ using SystemeSuiviProjets.SharedKernel;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class ProfessionnelEstDansCatégorie(Professionnel professionnel, Catégorie catégorie, DateOnly dateAffectation)
+    public class ProfessionnelEstDansCatégorie
         : BaseEntity
     {
-        public int ProfessionnelId { get; set; } = professionnel.Id;
-        public Professionnel Professionnel { get; private set; } = professionnel;
+        public int ProfessionnelId { get; set; }
+        public Professionnel Professionnel { get; private set; }
 
-        public int CatégorieId { get; set; } = catégorie.Id;
-        public Catégorie Catégorie { get; private set; } = catégorie;
+        public int CatégorieId { get; set; }
+        public Catégorie Catégorie { get; private set; }
 
-        public DateOnly DateAffectation { get; private set; } = dateAffectation;
+        public DateOnly DateAffectation { get; private set; }
+
+        public ProfessionnelEstDansCatégorie()
+        {
+            // EF Core needs this one to materialize collections from DB Set
+        }
+
+        public ProfessionnelEstDansCatégorie(Professionnel professionnel, Catégorie catégorie, DateOnly dateAffectation)
+        {
+            ProfessionnelId = professionnel.Id;
+            Professionnel = professionnel;
+            CatégorieId = catégorie.Id;
+            Catégorie = catégorie;
+            DateAffectation = dateAffectation;
+        }
     }
 }
