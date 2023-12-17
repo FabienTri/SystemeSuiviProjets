e0f1dc4f7fe170971d5a84db8b1b8e9abc7a2300
FabienTri <airaudfabien@gmail.com>
2023-12-17 09:33:26 -0500

    Add Gestionnaire constructor to extend Employé

--- SystemeSuiviProjets.Core/Entities/Gestionnaire.cs
+++ SystemeSuiviProjets.Core/Entities/Gestionnaire.cs
@@ -1,6 +1,9 @@
-﻿namespace SystemeSuiviProjets.Core
+﻿using System;
+
+namespace SystemeSuiviProjets.Core
 {
-    public class Gestionnaire : Employé
+    public class Gestionnaire(string nom, string prénom, string adresse, string téléphone, DateOnly dateEmbauche)
+        : Employé(nom, prénom, adresse, téléphone, dateEmbauche)
     {
     }
 }
