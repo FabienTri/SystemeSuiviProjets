f71d8fe5293c3d8865da5c641da46cd8a4df9540
FabienTri <airaudfabien@gmail.com>
2023-12-17 10:13:09 -0500

    Add Professionnel and Projet in EstAssigné

--- SystemeSuiviProjets.Core/Entities/EstAssigné.cs
+++ SystemeSuiviProjets.Core/Entities/EstAssigné.cs
@@ -3,8 +3,14 @@ using SystemeSuiviProjets.SharedKernel;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class EstAssigné(DateOnly dateAssignation) : BaseEntity
+    public class EstAssigné(Professionnel professionnel, Projet projet, DateOnly dateAssignation) : BaseEntity
     {
+        public int ProfessionnelId { get; set; } = professionnel.Id;
+        public Professionnel Professionnel { get; set; } = professionnel;
+
+        public int ProjetId { get; set; } = projet.Id;
+        public Projet Projet { get; set; } = projet;
+
         public DateOnly DateAssignation { get; set; } = dateAssignation;
     }
 }
