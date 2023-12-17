7f25dbb2bb85ce2b470ed0d85327e45133867b3a
FabienTri <airaudfabien@gmail.com>
2023-12-17 12:09:53 -0500

    Can't change SystèmePaiementProfessionnels and DateCréation

--- SystemeSuiviProjets.Core/Entities/FeuillePrésence.cs
+++ SystemeSuiviProjets.Core/Entities/FeuillePrésence.cs
@@ -4,14 +4,15 @@ using SystemeSuiviProjets.SharedKernel;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class FeuillePrésence(SystèmePaiementProfessionnels systèmePaiementProfessionnels, DateRange période, DateTime dateCréation)
+    public class FeuillePrésence(SystèmePaiementProfessionnels systèmePaiementProfessionnels,
+        DateRange période, DateTime dateCréation)
         : BaseEntity
     {
         public int SystèmePaiementProfessionnelsId { get; set; } = systèmePaiementProfessionnels.Id;
-        public SystèmePaiementProfessionnels SystèmePaiementProfessionnels { get; set; } = systèmePaiementProfessionnels;
+        public SystèmePaiementProfessionnels SystèmePaiementProfessionnels { get; private set; } = systèmePaiementProfessionnels;
 
         public DateRange Période { get; set; } = période;
-        public DateTime DateCréation { get; set; } = dateCréation;
+        public DateTime DateCréation { get; private set; } = dateCréation;
         public DateTime DateDernièreModification { get; set; } = dateCréation;
 
         public List<LigneFeuillePrésence> LignesFeuillePrésence { get; set; } = [];
