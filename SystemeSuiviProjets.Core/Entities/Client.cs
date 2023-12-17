e7bd4454f57d15bb9ade769c555f8444796e6df2
FabienTri <airaudfabien@gmail.com>
2023-12-17 12:01:14 -0500

    Can change Projets and initialize SystèmeFacturationClient

--- SystemeSuiviProjets.Core/Entities/Client.cs
+++ SystemeSuiviProjets.Core/Entities/Client.cs
@@ -2,15 +2,32 @@
 
 namespace SystemeSuiviProjets.Core
 {
-    public class Client(string nomConnexion, string motDePasse, string nomCompagnie, string adresse, string téléphone)
-        : Utilisateur(nomConnexion, motDePasse)
+    public class Client : Utilisateur
     {
-        public string NomCompagnie { get; set; } = nomCompagnie;
-        public string Adresse { get; set; } = adresse;
-        public string Téléphone { get; set; } = téléphone;
+        public string NomCompagnie { get; set; }
+        public string Adresse { get; set; }
+        public string Téléphone { get; set; }
 
-        public List<Projet> Projets { get; set; } = [];
+        public List<Projet> Projets { get; private set; }
+        public void AddProjet(Projet projet)
+        {
+            Projets.Add(projet);
+        }
+        public void RemoveProjet(Projet projet)
+        {
+            Projets.Remove(projet);
+        }
 
-        public SystèmeFacturationClients SystèmeFacturationClients { get; set; }
+        public SystèmeFacturationClients SystèmeFacturationClients { get; private set; }
+
+        public Client(string nomConnexion, string motDePasse, string nomCompagnie, string adresse, string téléphone)
+            : base(nomConnexion, motDePasse)
+        {
+            NomCompagnie = nomCompagnie;
+            Adresse = adresse;
+            Téléphone = téléphone;
+            Projets = [];
+            SystèmeFacturationClients = new SystèmeFacturationClients(this);
+        }
     }
 }
