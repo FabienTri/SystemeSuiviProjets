2242dd2f8b9a18d47ad02edde9d991e4b53b5dfa
FabienTri <airaudfabien@gmail.com>
2023-12-17 12:21:16 -0500

    Can change Catégories and Projets and can't change SystèmePaiementProfessionnels

--- SystemeSuiviProjets.Core/Entities/Professionnel.cs
+++ SystemeSuiviProjets.Core/Entities/Professionnel.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class Professionnel(string nomConnexion, string motDePasse, string nom, string prénom,
-        string adresse, string téléphone, DateOnly dateEmbauche)
-        : Employé(nomConnexion, motDePasse, nom, prénom, adresse, téléphone, dateEmbauche)
+    public class Professionnel : Employé
     {
-        public List<ProfessionnelEstDansCatégorie> Catégories { get; set; } = [];
+        public List<ProfessionnelEstDansCatégorie> Catégories { get; private set; }
+        public void AddProfessionnelEstDansCatégorie(ProfessionnelEstDansCatégorie professionnelEstDansCatégorie)
+        {
+            Catégories.Add(professionnelEstDansCatégorie);
+        }
+        public void RemoveProfessionnelEstDansCatégorie(ProfessionnelEstDansCatégorie professionnelEstDansCatégorie)
+        {
+            Catégories.Remove(professionnelEstDansCatégorie);
+        }
 
-        public List<EstAssigné> Projets { get; set; } = [];
+        public List<EstAssigné> Projets { get; private set; }
+        public void AddEstAssigné(EstAssigné estAssigné)
+        {
+            Projets.Add(estAssigné);
+        }
+        public void RemoveEstAssigné(EstAssigné estAssigné)
+        {
+            Projets.Remove(estAssigné);
+        }
 
-        public SystèmePaiementProfessionnels SystèmePaiementProfessionnels { get; set; }
+        public SystèmePaiementProfessionnels SystèmePaiementProfessionnels { get; private set; }
+
+        public Professionnel(string nomConnexion, string motDePasse, string nom, string prénom,
+                       string adresse, string téléphone, DateOnly dateEmbauche)
+            : base(nomConnexion, motDePasse, nom, prénom, adresse, téléphone, dateEmbauche)
+        {
+            Catégories = [];
+            Projets = [];
+            SystèmePaiementProfessionnels = new SystèmePaiementProfessionnels(this);
+        }
     }
 }
