b490b13bb25e4baec8ca499c97583f892021e9cb
FabienTri <airaudfabien@gmail.com>
2023-12-17 18:34:26 -0500

    Change constructor to add an empty one for EF core

--- SystemeSuiviProjets.Core/Entities/Session.cs
+++ SystemeSuiviProjets.Core/Entities/Session.cs
@@ -3,12 +3,24 @@ using SystemeSuiviProjets.SharedKernel;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class Session(Utilisateur utilisateur, DateTime dateConnexion) : BaseEntity
+    public class Session : BaseEntity
     {
-        public int UtilisateurId { get; set; } = utilisateur.Id;
-        public Utilisateur Utilisateur { get; private set; } = utilisateur;
+        public int UtilisateurId { get; set; }
+        public Utilisateur Utilisateur { get; private set; }
 
-        public DateTime DateConnexion { get; private set; } = dateConnexion;
+        public DateTime DateConnexion { get; private set; }
         public DateTime DateDeconnexion { get; set; }
+
+        public Session()
+        {
+            // EF Core needs this one to materialize collections from DB Set
+        }
+
+        public Session(Utilisateur utilisateur, DateTime dateConnexion)
+        {
+            UtilisateurId = utilisateur.Id;
+            Utilisateur = utilisateur;
+            DateConnexion = dateConnexion;
+        }
     }
 }
