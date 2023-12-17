ad5df30d3eac381f3186745c39dd9dfc2cb4a129
FabienTri <airaudfabien@gmail.com>
2023-12-17 18:27:50 -0500

    Change constructor to add an empty one for EF core

--- SystemeSuiviProjets.Core/Entities/LigneFeuillePrésence.cs
+++ SystemeSuiviProjets.Core/Entities/LigneFeuillePrésence.cs
@@ -2,17 +2,35 @@
 
 namespace SystemeSuiviProjets.Core
 {
-    public class LigneFeuillePrésence(FeuillePrésence feuillePrésence, Projet projet, TypeTarification typeTarification, int nbUnités, double fraisDéplacement = 0, double fraisSéjour = 0) : BaseEntity
+    public class LigneFeuillePrésence : BaseEntity
     {
-        public int FeuillePrésenceId { get; set; } = feuillePrésence.Id;
-        public FeuillePrésence FeuillePrésence { get; private set; } = feuillePrésence;
+        public int FeuillePrésenceId { get; set; }
+        public FeuillePrésence FeuillePrésence { get; private set; }
 
-        public int ProjetId { get; set; } = projet.Id;
-        public Projet Projet { get; private set; } = projet;
+        public int ProjetId { get; set; }
+        public Projet Projet { get; private set; }
 
-        public TypeTarification TypeTarification { get; set; } = typeTarification;
-        public int NombreUnités { get; set; } = nbUnités;
-        public double FraisDéplacement { get; set; } = fraisDéplacement;
-        public double FraisSéjour { get; set; } = fraisSéjour;
+        public TypeTarification TypeTarification { get; set; }
+        public int NombreUnités { get; set; }
+        public double FraisDéplacement { get; set; }
+        public double FraisSéjour { get; set; }
+
+        public LigneFeuillePrésence()
+        {
+            // EF Core needs this one to materialize collections from DB Set
+        }
+
+        public LigneFeuillePrésence(FeuillePrésence feuillePrésence, Projet projet, TypeTarification typeTarification,
+            int nbUnités, double fraisDéplacement = 0, double fraisSéjour = 0)
+        {
+            FeuillePrésenceId = feuillePrésence.Id;
+            FeuillePrésence = feuillePrésence;
+            ProjetId = projet.Id;
+            Projet = projet;
+            TypeTarification = typeTarification;
+            NombreUnités = nbUnités;
+            FraisDéplacement = fraisDéplacement;
+            FraisSéjour = fraisSéjour;
+        }
     }
 }
