a84e40c88a7b1ea1dd0fd06bc8c6c19c7f57d6d1
FabienTri <airaudfabien@gmail.com>
2023-12-17 09:56:14 -0500

    Add Professionnel and Catégorie in ProfessionnelEstDansCatégorie

--- SystemeSuiviProjets.Core/Entities/Professionnel.cs
+++ SystemeSuiviProjets.Core/Entities/Professionnel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SystemeSuiviProjets.Core
 {
     public class Professionnel(string nom, string prénom, string adresse, string téléphone, DateOnly dateEmbauche)
         : Employé(nom, prénom, adresse, téléphone, dateEmbauche)
     {
+        public List<ProfessionnelEstDansCatégorie> EstDansCatégories { get; set; }
     }
 }
