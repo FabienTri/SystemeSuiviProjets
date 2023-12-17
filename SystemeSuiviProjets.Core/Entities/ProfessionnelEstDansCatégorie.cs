7e5d15e23dfbf368bdf00c89a4c16d92813437fd
FabienTri <airaudfabien@gmail.com>
2023-12-17 10:00:06 -0500

    Add Professionnel and Catégorie in ProfessionnelEstDansCatégorie

--- SystemeSuiviProjets.Core/Entities/ProfessionnelEstDansCatégorie.cs
+++ SystemeSuiviProjets.Core/Entities/ProfessionnelEstDansCatégorie.cs
@@ -3,8 +3,15 @@ using SystemeSuiviProjets.SharedKernel;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class ProfessionnelEstDansCatégorie(DateOnly dateAffectation) : BaseEntity
+    public class ProfessionnelEstDansCatégorie(Professionnel professionnel, Catégorie catégorie, DateOnly dateAffectation)
+        : BaseEntity
     {
+        public int ProfessionnelId { get; set; } = professionnel.Id;
+        public Professionnel Professionnel { get; set; } = professionnel;
+
+        public int CatégorieId { get; set; } = catégorie.Id;
+        public Catégorie Catégorie { get; set; } = catégorie;
+
         public DateOnly DateAffectation { get; set; } = dateAffectation;
     }
 }
