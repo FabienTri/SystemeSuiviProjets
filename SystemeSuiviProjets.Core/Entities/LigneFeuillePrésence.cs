575a87d7b321bac071ab1023a47819b4241fe1f5
FabienTri <airaudfabien@gmail.com>
2023-12-17 12:15:22 -0500

    Can't change FeuillePrésence and Projet in LigneFeuillePrésence

--- SystemeSuiviProjets.Core/Entities/LigneFeuillePrésence.cs
+++ SystemeSuiviProjets.Core/Entities/LigneFeuillePrésence.cs
@@ -5,10 +5,10 @@ namespace SystemeSuiviProjets.Core
     public class LigneFeuillePrésence(FeuillePrésence feuillePrésence, Projet projet, TypeTarification typeTarification, int nbUnités, double fraisDéplacement = 0, double fraisSéjour = 0) : BaseEntity
     {
         public int FeuillePrésenceId { get; set; } = feuillePrésence.Id;
-        public FeuillePrésence FeuillePrésence { get; set; } = feuillePrésence;
+        public FeuillePrésence FeuillePrésence { get; private set; } = feuillePrésence;
 
         public int ProjetId { get; set; } = projet.Id;
-        public Projet Projet { get; set; } = projet;
+        public Projet Projet { get; private set; } = projet;
 
         public TypeTarification TypeTarification { get; set; } = typeTarification;
         public int NombreUnités { get; set; } = nbUnités;
