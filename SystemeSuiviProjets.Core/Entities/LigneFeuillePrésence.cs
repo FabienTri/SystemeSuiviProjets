e1390b05b61432591a1a0862fd944aac7049fc13
FabienTri <airaudfabien@gmail.com>
2023-12-17 11:06:34 -0500

    Add FeuillePrésence and Projet to LigneFeuillePrésence

--- SystemeSuiviProjets.Core/Entities/LigneFeuillePrésence.cs
+++ SystemeSuiviProjets.Core/Entities/LigneFeuillePrésence.cs
@@ -2,8 +2,14 @@
 
 namespace SystemeSuiviProjets.Core
 {
-    public class LigneFeuillePrésence(TypeTarification typeTarification, int nbUnités, double fraisDéplacement = 0, double fraisSéjour = 0) : BaseEntity
+    public class LigneFeuillePrésence(FeuillePrésence feuillePrésence, Projet projet, TypeTarification typeTarification, int nbUnités, double fraisDéplacement = 0, double fraisSéjour = 0) : BaseEntity
     {
+        public int FeuillePrésenceId { get; set; } = feuillePrésence.Id;
+        public FeuillePrésence FeuillePrésence { get; set; } = feuillePrésence;
+
+        public int ProjetId { get; set; } = projet.Id;
+        public Projet Projet { get; set; } = projet;
+
         public TypeTarification TypeTarification { get; set; } = typeTarification;
         public int NombreUnités { get; set; } = nbUnités;
         public double FraisDéplacement { get; set; } = fraisDéplacement;
