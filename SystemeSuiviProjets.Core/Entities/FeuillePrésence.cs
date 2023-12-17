00bcb451ae3b46abb8370356c0e9eb016805bf83
FabienTri <airaudfabien@gmail.com>
2023-12-17 10:59:42 -0500

    Add SystèmePaiementProfessionnel to FeuillePrésence

--- SystemeSuiviProjets.Core/Entities/FeuillePrésence.cs
+++ SystemeSuiviProjets.Core/Entities/FeuillePrésence.cs
@@ -3,8 +3,12 @@ using SystemeSuiviProjets.SharedKernel;
 
 namespace SystemeSuiviProjets.Core
 {
-    public class FeuillePrésence(DateRange période, DateTime dateCréation) : BaseEntity
+    public class FeuillePrésence(SystèmePaiementProfessionnels systèmePaiementProfessionnels, DateRange période, DateTime dateCréation)
+        : BaseEntity
     {
+        public int SystèmePaiementProfessionnelsId { get; set; } = systèmePaiementProfessionnels.Id;
+        public SystèmePaiementProfessionnels SystèmePaiementProfessionnels { get; set; } = systèmePaiementProfessionnels;
+
         public DateRange Période { get; set; } = période;
         public DateTime DateCréation { get; set; } = dateCréation;
         public DateTime DateDernièreModification { get; set; } = dateCréation;
