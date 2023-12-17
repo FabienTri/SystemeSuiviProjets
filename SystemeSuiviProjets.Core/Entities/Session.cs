d8d82fd93d56d0ba457fe42e45b288bfb1c4624c
FabienTri <airaudfabien@gmail.com>
2023-12-17 11:10:51 -0500

    Add UtilisateurId in Session

--- SystemeSuiviProjets.Core/Entities/Session.cs
+++ SystemeSuiviProjets.Core/Entities/Session.cs
@@ -5,6 +5,7 @@ namespace SystemeSuiviProjets.Core
 {
     public class Session(Utilisateur utilisateur, DateTime dateConnexion) : BaseEntity
     {
+        public int UtilisateurId { get; set; } = utilisateur.Id;
         public Utilisateur Utilisateur { get; set; } = utilisateur;
 
         public DateTime DateConnexion { get; set; } = dateConnexion;
