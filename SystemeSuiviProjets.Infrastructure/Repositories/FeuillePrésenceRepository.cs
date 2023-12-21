149a7bdb98e900ed682158ec6e547d9b446ea722
FabienTri <airaudfabien@gmail.com>
2023-12-21 23:04:40 +0100

    Correct feuille présence repository

---
+++ SystemeSuiviProjets.Infrastructure/Repositories/FeuillePrésenceRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemeSuiviProjets.Core;
+using SystemeSuiviProjets.Core.Interfaces;
+
+namespace SystemeSuiviProjets.Infrastructure.Repositories
+{
+    public class FeuillePrésenceRepository(SystèmeSuiviProjetsContext systemeSuiviProjetsContext)
+        : EfRepository<FeuillePrésence>(systemeSuiviProjetsContext), IFeuillePrésenceRepository
+    {
+        public FeuillePrésence GetByIdWithSessions(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FeuillePrésence> GetByIdWithSessionsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
