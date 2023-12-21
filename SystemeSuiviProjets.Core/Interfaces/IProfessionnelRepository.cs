909060c1734222c8331849072e5a29f06ae68e14
FabienTri <airaudfabien@gmail.com>
2023-12-21 11:01:14 +0100

    Add Professionnel Repository Interface

---
+++ SystemeSuiviProjets.Core/Interfaces/IProfessionnelRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using SystemeSuiviProjets.SharedKernel.Interfaces;
+
+namespace SystemeSuiviProjets.Core.Interfaces
+{
+    public interface IProfessionnelRepository : IAsyncRepository<Professionnel>, IRepository<Professionnel>
+    {
+        Task<Professionnel> GetByIdWithCatégoriesAsync(int id);
+        Professionnel GetByIdWithCatégories(int id);
+    }
+}
