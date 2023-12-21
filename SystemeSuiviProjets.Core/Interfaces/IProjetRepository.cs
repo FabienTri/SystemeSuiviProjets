3968ceb55564471c6ff72836dd28ba025d0ddae0
FabienTri <airaudfabien@gmail.com>
2023-12-21 11:07:36 +0100

    Create Projet repository interface

---
+++ SystemeSuiviProjets.Core/Interfaces/IProjetRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using SystemeSuiviProjets.SharedKernel.Interfaces;
+
+namespace SystemeSuiviProjets.Core.Interfaces
+{
+    public interface IProjetRepository : IAsyncRepository<Projet>, IRepository<Projet>
+    {
+        Task<Projet> GetByIdWithSessionsAsync(int id);
+        Projet GetByIdWithSessions(int id);
+    }
+}
