88776d1e32a4bcf7b8a6fa48967a0a0f0589d9ba
FabienTri <airaudfabien@gmail.com>
2023-12-21 11:07:48 +0100

    Create Utilisateur repository interface

---
+++ SystemeSuiviProjets.Core/Interfaces/IUtilisateurRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using SystemeSuiviProjets.SharedKernel.Interfaces;
+
+namespace SystemeSuiviProjets.Core.Interfaces
+{
+    public interface IUtilisateurRepository : IAsyncRepository<Utilisateur>, IRepository<Utilisateur>
+    {
+        Task<Utilisateur> GetByIdWithSessionsAsync(int id);
+        Utilisateur GetByIdWithSessions(int id);
+    }
+}
