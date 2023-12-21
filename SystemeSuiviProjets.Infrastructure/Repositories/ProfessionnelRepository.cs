e8276f81a8cac085b60ba46cc6300d9b36cb0547
FabienTri <airaudfabien@gmail.com>
2023-12-21 16:42:07 +0100

    Add professionnel repository

---
+++ SystemeSuiviProjets.Infrastructure/Repositories/ProfessionnelRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using SystemeSuiviProjets.Core;
+using SystemeSuiviProjets.Core.Interfaces;
+
+namespace SystemeSuiviProjets.Infrastructure.Repositories
+{
+    public class ProfessionnelRepository(SystèmeSuiviProjetsContext systemeSuiviProjetsContext)
+        : EfRepository<Professionnel>(systemeSuiviProjetsContext), IProfessionnelRepository
+    {
+        public Professionnel GetByIdWithCatégories(int id)
+        {
+            return _SystemeSuiviProjetsContext.Professionnels
+                .Include(p => p.Catégories)
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public Task<Professionnel> GetByIdWithCatégoriesAsync(int id)
+        {
+            return _SystemeSuiviProjetsContext.Professionnels
+                .Include(p => p.Catégories)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+    }
+}
