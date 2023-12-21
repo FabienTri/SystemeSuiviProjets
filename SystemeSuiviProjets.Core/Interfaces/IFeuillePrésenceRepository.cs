4ec8add19173ae9481c10e1a875121f2a129ba6d
FabienTri <airaudfabien@gmail.com>
2023-12-21 11:07:30 +0100

    Create Feuille Présence repository interface

---
+++ SystemeSuiviProjets.Core/Interfaces/IFeuillePrésenceRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using SystemeSuiviProjets.SharedKernel.Interfaces;
+
+namespace SystemeSuiviProjets.Core.Interfaces
+{
+    public interface IFeuillePrésenceRepository : IAsyncRepository<FeuillePrésence>, IRepository<FeuillePrésence>
+    {
+        Task<FeuillePrésence> GetByIdWithSessionsAsync(int id);
+        FeuillePrésence GetByIdWithSessions(int id);
+    }
+}
