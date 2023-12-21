ef6c8541f5ada0fd73da88a44a8a8606845125a5
FabienTri <airaudfabien@gmail.com>
2023-12-21 23:04:12 +0100

    Correct IAsyncRepository

--- SystemeSuiviProjets.SharedKernel/Interfaces/IAsyncRepository.cs
+++ SystemeSuiviProjets.SharedKernel/Interfaces/IAsyncRepository.cs
@@ -7,11 +7,8 @@ namespace SystemeSuiviProjets.SharedKernel.Interfaces
     public interface IAsyncRepository<T> where T : BaseEntity, IAggregateRoot
     {
         Task<T> GetByIdAsync(int id);
-        T GetById(int id);
         Task<IReadOnlyList<T>> ListAllAsync();
-        IReadOnlyList<T> ListAll();
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
-        IReadOnlyList<T> List(ISpecification<T> spec);
         Task<T> AddAsync(T entity);
         void UpdateAsync(T entity);
         //void UpdateEntity(T entity); // Added for bug "already attached"
